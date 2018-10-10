77478d066aa89532016514255010a37e642a6c53
Tomasz <tmajewski@poczta.pl>
2018-10-10 17:31:14 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,20 @@
+package pl.infoshare;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {   String name = "Tomasz";
+        String lastName = "Majewski";
+        byte age = 34;
+        String hometown= "Malbork";
+        int result = 10 + 5;
+        System.out.print(result);
+
+    }
+}
